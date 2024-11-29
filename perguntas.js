ef82576396a9dacea3b275364823fc1acc9739fa
Fabriciodograu_7 <00001094261543sp@al.educacao.sp.gov.br>
2024-11-28 23:49:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Física',
+    'O que é gravidade?',
+    'Gravidade é a força que atrai os objetos para o centro da Terra ou para qualquer corpo massivo.'
+)
+
+criaCartao(
+    'Culinária',
+    'Qual é o ingrediente principal do pão?',
+    'O ingrediente principal do pão é a farinha de trigo.'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é inteligência artificial?',
+    'Inteligência artificial é a simulação de inteligência humana por máquinas, especialmente computadores.'
+)
+
+criaCartao(
+    'Mitologia',
+    'Quem é Zeus na mitologia grega?',
+    'Zeus é o deus do trovão e o rei dos deuses na mitologia grega.'
+)
